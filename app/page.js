390051bc3e52d54baa8b0ddd7a1a3b803d69ff27
Preet Lakra <lakraaman007@gmail.com>
2023-07-16 10:26:48 +0530

    finally commit and added

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-start py-28 m-7 ">
+      <div className="flex flex-col mb-6">
+        <h1 className='text-3xl font-bold mb-3'>I'm Preet Lakra</h1>
+        <span>Full-Stack Engineer.</span>
+        <span>Freelance.</span>
+      </div>
+      <div className=''>
+        <h2 className='text-2xl font-bold mb-3 underline'>Skills</h2>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </main>
+  )
+}
